3fdddec9dfa97073860e136a68d9a115fcefe230
dimaematveev <dimaematveev@gmail.com>
2020-05-12 21:56:36 +0300

    Добавлен вертикальный прогресс  бар

--- SortAlgorithms/SortAlgorithms.WPF/MainWindow.xaml.cs
+++ SortAlgorithms/SortAlgorithms.WPF/MainWindow.xaml.cs
@@ -21,34 +21,69 @@ namespace SortAlgorithms.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly AlgorithmsBase<int> algorithm = new CocktailSort<int>();
+        List<SortedItem> items = new List<SortedItem>();
         public MainWindow()
         {
             InitializeComponent();
-            algorithm.ItemsEdit += Algorithm_ItemsEdit;
+            AddButon.Click += AddButon_Click;
+            FillButton.Click += Fill_Click;
+            //algorithm.ItemsEdit += Algorithm_ItemsEdit;
         }
 
-        private void Algorithm_ItemsEdit()
+        private void Fill_Click(object sender, RoutedEventArgs e)
         {
-            textbox3.Text += "\n" + MassToString(algorithm.Items);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.TryParse(texbox1.Text, out int value))
+            if (int.TryParse(FillTextBox.Text, out int value))
             {
-                algorithm.Items.Add(value);
-                label1.Content += " " + value.ToString();
+                var rnd = new Random();
+                for (int i = 0; i < value; i++)
+                {
+                    var item = new SortedItem(rnd.Next(0,100));
+                    items.Add(item);
+                    ProgressBars.Children.Add(item.Grid);
+                }
+                
             }
+            FillTextBox.Text = "";
         }
 
-        private void Button_Click1(object sender, RoutedEventArgs e)
+        private void AddButon_Click(object sender, RoutedEventArgs e)
         {
-            
-            textbox3.Text += "\n" + MassToString(algorithm.Items);
-            algorithm.Sort();
-            label2.Content += MassToString(algorithm.Items);
+            if (int.TryParse(AddTextBox.Text, out int value))
+            {
+                var item = new SortedItem(value);
+                items.Add(item);
+                ProgressBars.Children.Add(item.Grid);
+            }
+            AddTextBox.Text = "";
         }
+
+
+
+
+
+        //private void Algorithm_ItemsEdit()
+        //{
+        //    textbox3.Text += "\n" + MassToString(algorithm.Items);
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (int.TryParse(texbox1.Text, out int value))
+        //    {
+        //        algorithm.Items.Add(value);
+        //        label1.Content += " " + value.ToString();
+        //    }
+        //}
+
+        //private void Button_Click1(object sender, RoutedEventArgs e)
+        //{
+            
+        //    textbox3.Text += "\n" + MassToString(algorithm.Items);
+        //    algorithm.Sort();
+        //    label2.Content += MassToString(algorithm.Items);
+        //}
+
+
 
         private string MassToString(List<int> list, char separator =' ')
         {
---
+++ SortAlgorithms/SortAlgorithms.WPF/SortedItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SortAlgorithms.WPF
+{
+    public class SortedItem
+    {
+        public ProgressBar ProgressBar { get; private set; }
+        public Label Label { get; private set; }
+
+        public int Value { get; set; }
+
+        public Grid Grid { get; private set; }
+
+        public SortedItem(int value)
+        {
+            Value = value;
+            ProgressBar = new ProgressBar();
+            ProgressBar.Orientation = Orientation.Vertical;
+            ProgressBar.Minimum = 0;
+            ProgressBar.Maximum = 100;
+            ProgressBar.Value = Value;
+
+            Label = new Label();
+            Label.Content = Value.ToString();
+
+
+            Grid = new Grid();
+            Grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1,GridUnitType.Star) });
+            Grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(25) });
+            
+            
+
+            Grid.SetRow(ProgressBar,0);
+            Grid.Children.Add(ProgressBar);
+            Grid.SetRow(Label, 1);
+            Grid.Children.Add(Label);
+
+
+        }
+    }
+}
