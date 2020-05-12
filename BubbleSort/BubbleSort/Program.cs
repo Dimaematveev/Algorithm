c66fa65d5b7f0a290184dfe7ba96207a2a24f975
dimaematveev <dimaematveev@gmail.com>
2020-05-12 18:38:08 +0300

    Пустой проект и базовый класс для всех алгоритмов сортировки

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+   /// <summary>
+   /// Сортировка пузырьком
+   /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[] { 8, 4,5,5, 7, 1, 2 };
+            ConsoleMass(mass);
+            int len = mass.Length;
+            while (len>0)
+            {
+                for (int i = 0; i < len-1; i++)
+                {
+                    if (mass[i]>mass[i+1])
+                    {
+                        int k = mass[i];
+                        mass[i] = mass[i + 1];
+                        mass[i + 1] = k;
+
+                    }
+                }
+                len--;
+            }
+            ConsoleMass(mass);
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+
+        private static void ConsoleMass(int[] mas)
+        {
+            Console.WriteLine();
+            foreach (var item in mas)
+            {
+                Console.Write($"{item},");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SortAlgorithms/SortAlgorithms.BL/AlgorithmsBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SortAlgorithms.BL
+{
+    public class AlgorithmsBase<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+
+        public void Swop(int positionA,int positionB)
+        {
+            if (positionA<Items.Count && positionB<Items.Count)
+            {
+                var temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+            }
+        }
+    }
+}
